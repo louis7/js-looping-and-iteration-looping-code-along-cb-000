aa9bea25bc781243929ebe871702da2f4ef534aa
louis baptiste <louisvis@ymail.com>
2020-02-07 19:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function writeCards(names, specialEvent) {
   cards= [];
   for (let i = 0; i < names.length; i++) {
     cards.push('Thank you, ${names[i]}, for the wonderful ${specialEvent} gift!');
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
 
   }
 
